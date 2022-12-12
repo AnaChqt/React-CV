804369ba636c7ec524a9ae9bb9b8cdfa35de75fb
AnaChqt <anais.choquet@outlook.fr>
2022-12-12 18:27:36 +0100

    Setting up projects data

---
+++ src/data/DataPortfolio.jsx
@@ -0,0 +1,51 @@
+import Movies from '../assets/portfolio/Movies.png';
+import ShiFuMi from '../assets/portfolio/ShiFuMi.png';
+import Calendar from '../assets/portfolio/Calendar.png';
+import Form from '../assets/portfolio/Form.png';
+import ManuMedic from '../assets/portfolio/ManuMedic.png';
+import CV1 from '../assets/portfolio/CV1.png';
+import AtelierDouceur from '../assets/portfolio/AtelierDouceur.png';
+import Wanderlust from '../assets/portfolio/Wanderlust.png';
+
+export const projectsList = [
+    {
+        image: Wanderlust,
+        title: 'Wanderlust',
+        category: 'Symphony',
+    },
+    {
+        image: AtelierDouceur,
+        title: 'Atelier Douceur',
+        category: 'WordPress',
+    },
+    {
+        image: CV1,
+        title: 'Portfolio Version 1',
+        category: 'HTML/CSS/JS'
+    },
+    {
+        image: ManuMedic,
+        title: 'Manu Medic',
+        category: 'PHP'
+    },
+    {
+        image: Form,
+        title: 'Form',
+        category: 'PHP'
+    },
+    {
+        image: Calendar,
+        title: 'Calendar',
+        category: 'PHP'
+    },
+    {
+        image: ShiFuMi,
+        title: 'ShiFuMi Harry Potter',
+        category: 'HTML/CSS/JS'
+    },
+    {
+        image: Movies,
+        title: 'My Favorite Movies',
+        category: 'HTML/CSS/JS'
+    },
+];
