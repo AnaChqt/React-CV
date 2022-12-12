c42fb2276f5135fa0e0a6fa041797a686c2f5a5b
AnaChqt <anais.choquet@outlook.fr>
2022-12-12 17:56:57 +0100

    Setting up the App page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+// CSS global
 import './App.css';
+// Add components
+import NavbarExpand from './components/Navbar';
+// Router
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+// Add pages
+import Home from './pages/home/Home';
+import About from './pages/about/About';
+import Works from './pages/works/Works';
+import Contact from './pages/contact/Contact';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <NavbarExpand />
+                <Routes>
+                    <Route exact path='/' element={<Home />} />
+                    <Route path='/about' element={<About/>} />
+                    <Route path='/works' element={<Works/>} />
+                    <Route path='/contact' element={<Contact/>} />
+                </Routes>
+        </Router>
+    )
 }
 
-export default App;
+export default App
