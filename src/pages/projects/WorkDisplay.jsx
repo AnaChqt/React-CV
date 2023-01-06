ce4e1f16d9d0e0455a6b4150e6ab54b8b4e69c04
AnaChqt <anais.choquet@outlook.fr>
2023-01-06 14:49:20 +0100

    Update portfolio with img and info + style

--- src/pages/projects/WorkDisplay.jsx
+++ src/pages/projects/WorkDisplay.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 // CSS
 import './WorkDisplay.css';
 // Bootstrap
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 // Route
 import { useParams } from 'react-router-dom';
 // Data
@@ -23,16 +23,25 @@ const WorkDisplay = () => {
             </Container>
 
             <Container fluid className="my-5 d-flex justify-content-center text-center">
-                <Row>
-                    <Col xs={12} lg={6}>
-                        <h3>{ project.title }</h3>
-                    </Col>
-                    <Col xs={12} lg={6}>
-                        <h3>{ project.description1 }</h3>
-                    </Col>
-                    <Col xs={12} lg={6}>
-                        <h3>{ project.title }</h3>
-                    </Col>
+                <Row className="d-flex">
+                    <Col xs={12} lg={7} className="d-flex justify-content-center align-items-center ">
+                        <img className="display" src={ project.first_display } alt={ project.title } />
+                    </Col> 
+
+					<Col xs={12} lg={5} className="text-center ">
+                        <p className="p-3">{ project.description1 }</p>
+                        <p className="p-3">{ project.description2 }</p>
+					</Col>
+                </Row>
+            </Container>
+
+            <Container fluid className="my-5 d-flex justify-content-center text-center">
+                <Row className="mt-5 d-flex justify-content-center align-items-center">
+                    <h3 className='mb-3'>Version Responsive</h3>
+                    <img className="responsive" src={ project.responsive } alt={ project.title } />
+                    <div className="text-center py-5">
+						<a href={ project.demo } target="_blank" rel="noopener noreferrer" ><Button className="btnCV" type="submit">Découvrir le projet</Button></a>                
+					</div>
                 </Row>
             </Container>
         </>
