e4a1543e79c473edfc85c5167202295fb8760bc9
AnaChqt <anais.choquet@outlook.fr>
2023-01-08 18:28:43 +0100

    Clean code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
-// Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
-// CSS global
 import './App.css';
-// Add components
 import NavbarExpand from './components/Navbar';
 import Footer from './components/Footer';
-// Router
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-// Add pages
 import Home from './pages/home/Home';
 import About from './pages/about/About';
 import Works from './pages/works/Works';
