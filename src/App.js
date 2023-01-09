3cdcdeb9a18fcd5e45fb619731855930b85be392
AnaChqt <anais.choquet@outlook.fr>
2023-01-09 11:46:59 +0100

    test path

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 import NavbarExpand from './components/Navbar';
 import Footer from './components/Footer';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Home from './components/home/Home';
+import Home from './components/home';
 import About from './pages/about/About';
 import Works from './pages/works/Works';
 import WorkDisplay from './pages/projects/WorkDisplay';
@@ -16,7 +16,7 @@ function App() {
         <Router>
             <NavbarExpand />
                 <Routes>
-                    <Route exact path='/' element={<Home />} />
+                    <Route path='/' element={<Home />} />
                     <Route path='/about' element={<About/>} />
                     <Route path='/works' element={<Works/>} />
                     <Route path='/work/:id' element={<WorkDisplay/>} />
