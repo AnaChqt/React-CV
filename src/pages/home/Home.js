b57563070f530dd3357fdd7b19437b64a7e48464
AnaChqt <anais.choquet@outlook.fr>
2023-02-06 17:25:08 +0100

    New link for LinkedIn

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -15,7 +15,7 @@ const Home = () => {
 						<h1 className="mx-1 mt-5">HELLO, WORLD !</h1>
 						<p className="biographie mt-4">Je suis Anaïs Choquet, <span className="jobName fw-bold">Front-End Developper</span></p>                       
 						<div className="d-flex justify-content-center py-4">
-							<a href="https://www.linkedin.com/in/ana%C3%AFs-choquet-00926a158/" target="_blank" rel="noopener noreferrer"><img src={ linkedin } className="social" id="linkedin" alt="Logo Linkedin"/></a>
+							<a href="https://www.linkedin.com/in/ana%C3%AFs-choquet/" target="_blank" rel="noopener noreferrer"><img src={ linkedin } className="social" id="linkedin" alt="Logo Linkedin"/></a>
 							<a href="https://github.com/AnaChqt" target="_blank" rel="noopener noreferrer"><img src={ github } className="social" id="github" alt="Logo GitHub"/></a>
 						</div>
 						<div className="text-center py-4">
